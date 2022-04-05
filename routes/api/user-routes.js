5b62a6716028b948a6366120a1295ba882370489
Chris-McLeod2 <91762149+Chris-McLeod2@users.noreply.github.com>
2022-04-05 16:50:57 -0500

    router logic tenatively complete

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,31 @@
 const router = require('express').Router();
+
+const {
+getAllUser,
+getUserById,
+createUser,
+updateUser,
+deleteUser,
+addFriend,
+deleteFriend
+} = require('../../controllers/user-controller.js')
+
+router
+.route('/')
+.get(getAllUser)
+.post(createUser)
+
+
+
+router
+.route('/:id')
+.get(getUserById)
+.delete(deleteUser)
+.put(updateUser)
+
+router
+.route('/:userID/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend)
+
+module.exports = router;
