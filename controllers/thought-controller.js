d14fe80c650356baad47da91518705e5b056dc43
Chris-McLeod2 <91762149+Chris-McLeod2@users.noreply.github.com>
2022-04-07 14:53:36 -0500

    get/create thought routes made

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,14 +1,15 @@
+const res = require('express/lib/response');
 const {Thought, User} = require('../models');
 
 
 // router
 // .route('/') 
-// .get(getAllThought)
-// .post(createThought)
+// .get(getAllThought) done
+// .post(createThought) done
 
 // router
-// .route('/:id')
-// .get(getThoughtById)
+// .route('/:id') 
+// .get(getThoughtById) done
 // .put(updateThought)
 // .delete(deleteThought)
 
@@ -17,9 +18,60 @@ const {Thought, User} = require('../models');
 // .post(createReaction)
 // .route('/:thoughtId/reactions/:reactionId')
 // .delete(deleteReaction)
+
+
 const thoughtController = {
 
+    getAllThought(req, res) {
+        Thought.find({})
+          .populate({
+            path: 'reactions',
+            select: '-__v'
+          })
+          .select('-__v')
+          .sort({ _id: -1 })
+          .then(dbThoughtData => res.json(dbThoughtData))
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
 
+      createThought({ body }, res) {
+        Thought.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: body.userId },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
+    },
+
+      
+
+      getThoughtById(req, res) {
+        Thought.find({})
+          .populate({
+            path: 'reactions',
+            select: '-__v'
+          })
+          .select('-__v')
+          .sort({ _id: -1 })
+          .then(dbThoughtData => res.json(dbThoughtData))
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
 
 }
 
