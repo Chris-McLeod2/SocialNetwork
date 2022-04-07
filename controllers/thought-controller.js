86c03cb18294b74c50a868584b1675dacd781842
Chris-McLeod2 <91762149+Chris-McLeod2@users.noreply.github.com>
2022-04-07 14:34:58 -0500

    thought controller setup

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,5 +1,22 @@
 const {Thought, User} = require('../models');
 
+
+// router
+// .route('/') 
+// .get(getAllThought)
+// .post(createThought)
+
+// router
+// .route('/:id')
+// .get(getThoughtById)
+// .put(updateThought)
+// .delete(deleteThought)
+
+// router
+// .route('/:thoughtId/reactions')
+// .post(createReaction)
+// .route('/:thoughtId/reactions/:reactionId')
+// .delete(deleteReaction)
 const thoughtController = {
 
 
